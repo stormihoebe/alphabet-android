069a4365fe2480a81d6f5220b095bf549776796b
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-05-24 09:50:14 -0700

    initial commit with main activity and basic grid adapter for alphabet

---
+++ app/src/main/java/com/example/guest/newapp/AlphabetAdapter.java
@@ -0,0 +1,64 @@
+package com.example.guest.newapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by Guest on 5/24/17.
+ */
+
+public class AlphabetAdapter extends BaseAdapter{
+    private Context mContext;
+    private String[] mLetters;
+
+
+    public AlphabetAdapter (Context context, String[] letters) {
+        this.mContext = context;
+        this.mLetters = letters;
+    }
+
+    @Override
+    public int getCount() {
+        return mLetters.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        String letter = mLetters[position];
+        return letter;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View gridView;
+
+        if (convertView == null) {
+            String letter = (String) this.getItem(position);
+            // get layout from xml file
+            gridView = inflater.inflate(R.layout.alphabet_grid_item, null);
+
+
+            // pull views
+            TextView letterView = (TextView) gridView
+                    .findViewById(R.id.grid_item_letter);
+
+            // set values into views
+            letterView.setText(letter);
+        } else {
+            gridView = (View) convertView;
+        }
+        return gridView;
+        }
+    }
